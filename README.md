4e9889a42c7daa99f7e5ab837c7a60018fe4f165
Shay Elkin <shayelkin@users.noreply.github.com>
2023-11-28 19:40:43 -0800

    First and probably final version

---
+++ README.md
@@ -0,0 +1,20 @@
+# What a Week, Huh?
+
+A silly bot that updates the "What a Week?" meme from "30 Rock" to match
+the current day, and posts the results to bluesky.
+
+Uses [serverless.com](https://serverless.com/) for deployment and configuration.
+
+## Attributions
+
+* `handler.py` contains code from [create_bsky_post.py](https://github.com/bluesky-social/atproto-website/blob/main/examples/create_bsky_post.py), 
+  copyright 2022-2023 Bluesky PBC, and Contributors, licensed under the 
+  [Creative Commons Attribution (CC-BY) license](https://github.com/bluesky-social/atproto-website/blob/main/LICENSE-CC-BY.txt).
+
+* The file `rsrc/OpenSans-Bold.ttf` is Copyright 2020 [The Open Sans Project Authors](https://github.com/googlefonts/opensans), 
+  licensed under the [Open Font License](https://github.com/googlefonts/opensans/blob/main/OFL.txt)
+
+* The file `rsrc/lemon-template.jpg` is based on frames from the TV show 30 Rock, 
+  copyright 2009 NBC Universal, Inc. and others. Usage here is assumed to be "fair 
+  use".
+
---
+++ handler.py
@@ -0,0 +1,157 @@
+
+from collections import namedtuple
+import datetime
+import json
+import io
+import logging
+import tempfile
+from typing import Optional, Dict, List
+import os
+
+from bs4 import BeautifulSoup
+from PIL import Image, ImageDraw, ImageFont
+import pytz
+import requests
+
+ATP_PDS_HOST = "https://bsky.social"
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+### Mostly copied from create_bsky_post.py because I can't be bothered.
+
+def bsky_login_session(pds_url: str, handle: str, password: str) -> Dict:
+    resp = requests.post(
+        pds_url + "/xrpc/com.atproto.server.createSession",
+        json={"identifier": handle, "password": password},
+    )
+    resp.raise_for_status()
+    return resp.json()
+
+def upload_file(pds_url, access_token, mimetype, img_bytes) -> Dict:
+    resp = requests.post(
+        pds_url + "/xrpc/com.atproto.repo.uploadBlob",
+        headers={
+            "Content-Type": mimetype,
+            "Authorization": "Bearer " + access_token,
+        },
+        data=img_bytes,
+    )
+    resp.raise_for_status()
+    return resp.json()["blob"]
+
+def upload_images(pds_url: str, access_token: str, fd: io.IOBase , alt_text: str) -> Dict[str, str]:
+    fd.seek(0)    
+    img_bytes = fd.read()
+    # this size limit specified in the app.bsky.embed.images lexicon
+    assert len(img_bytes) <= 1000000
+
+    blob = upload_file(pds_url, access_token, "image/jpeg", img_bytes)
+
+    return {
+        "$type": "app.bsky.embed.images",
+        "images": [{"alt": alt_text, "image": blob}],
+    }
+
+def create_post(pds_url: str, handle: str, password: str, image_file: io.IOBase, alt_text: str, dry_run: bool = False):
+    session = bsky_login_session(pds_url, handle, password)
+    # trailing "Z" is preferred over "+00:00"
+    now = datetime.datetime.now(datetime.timezone.utc).isoformat().replace("+00:00", "Z")
+
+    # these are the required fields which every post must include
+    post = {
+        "$type": "app.bsky.feed.post",
+        "text": "",
+        "createdAt": now,
+    }
+
+    post["embed"] = upload_images(pds_url, session["accessJwt"], image_file, alt_text)
+
+    logger.info("Creating post: %s", json.dumps(post, indent=None))
+
+    if not dry_run:
+        resp = requests.post(
+            pds_url + "/xrpc/com.atproto.repo.createRecord",
+            headers={"Authorization": "Bearer " + session["accessJwt"]},
+            json={
+                "repo": session["did"],
+                "collection": "app.bsky.feed.post",
+                "record": post,
+            },
+        )
+
+        logger.info("createRecord response: %s", json.dumps(resp.json(), indent=None))
+        resp.raise_for_status()
+
+#### End create_bsky_post.py
+
+def day(zone: Optional[str] = "US/Pacific") -> str:
+    tz = pytz.timezone(zone)
+    return datetime.datetime.now(tz).strftime("%A")
+
+MemeTemplate = namedtuple("MemeTemplate", ["image_file", "font_file", "font_size", "top", "left", "min_width", "color", "bg_color"])
+
+def create_image(template: MemeTemplate, text: str) -> io.IOBase:
+    image = Image.open(template.image_file)
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.truetype(font=template.font_file, size=template.font_size)
+
+    _, _, text_width, text_height = font.getbbox(text)
+    text_width = max(text_width, template.min_width)
+    draw.rectangle((template.top, template.left, template.top+text_width, template.left+text_height), fill=template.bg_color)
+    draw.text((template.top, template.left), text, font=font, fill=template.color, background=template.bg_color)
+
+    tf = tempfile.TemporaryFile()
+    image.save(fp=tf, format="jpeg")
+    logger.info("Wrote image: %d bytes", tf.tell())
+
+    return tf
+    
+def deserialize_color(color: str) -> tuple:
+    res = tuple(int(c.strip()) for c in color.split(',', maxsplit=2))
+    assert len(res) == 3
+    return res
+
+def deserialize_meme_template(template: dict, base_path: Optional[str] = "./") -> MemeTemplate:
+    return MemeTemplate(
+        image_file=os.path.join(base_path, template["image_file"]),        
+        font_file=os.path.join(base_path, template["font_file"]),
+        font_size=template["font_size"],
+        top=template["top"],
+        left=template["left"],
+        min_width=template["min_width"],
+        color=deserialize_color(template["color"]),
+        bg_color=deserialize_color(template["bg_color"]),
+    )
+
+def create_meme_image(template_fname: str, text: str) -> io.IOBase:
+    base_path = os.path.dirname(template_fname)
+
+    with open(template_fname) as f:
+        template = deserialize_meme_template(json.load(f), base_path=base_path)
+        return create_image(template, text)
+
+def get_username() -> str:
+    username = os.environ.get('BSKY_USERNAME')
+    if not username:
+        raise Exception("BSKY_USERNAME not set")
+
+def get_password() -> str:
+    password = os.environ.get('BSKY_PASSWORD')
+    if not username:
+        raise Exception("BSKY_PASSWORD not set")
+
+def get_env(name: str) -> str:
+    val = os.environ.get(name)
+    if not val:
+        raise Exception(f"{name} not set")
+    return val
+
+def run(event, context):
+    day_name = day()
+    image = create_meme_image("rsrc/template.json", day_name)
+    alt_text = "Liz Lemon (Tina Fey), complains to character Jack Donaghy (Alec Baldwin), " \
+        + "about having finished a hard week of work, with Donaghey reminding her that it is " \
+        + "still " + day_name
+    create_post(ATP_PDS_HOST, get_env('BSKY_USERNAME'), get_env('BSKY_PASSWORD'), image, alt_text, False)
